bf07aa4c4c632ba3d46c8dc9e7d249807f96f189
logwindy4 <logwindy4@gmail.com>
2023-10-19 15:20:46 +0900

    테스트

---
+++ test/src/main/java/test/report1.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.util.Scanner;
+
+public class report1 {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+
+            System.out.print("인원수를 입력하세요 : ");
+            int random = sc.nextInt();
+            String[] randomPeople = new String[random];
+
+
+            for (int i = 0; i < randomPeople.length; i++) {
+                System.out.print("참여자의 이름을 입력하세요 : ");
+                randomPeople[i] = sc.next();
+            }
+
+
+            int winner = (int) (Math.random() * randomPeople.length);
+            String win = randomPeople[winner];
+            System.out.println();
+            System.out.println("  $$몰빵$$ !!당첨자는!! " + win + " !!입니다!!");
+            System.out.println(".....................................");
+            System.out.println("⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢠⣴⣾⣿⣶⣶⣆⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀");
+            System.out.println("⢀⢀⢀⣀⢀⣤⢀⢀⡀⢀⣿⣿⣿⣿⣷⣿⣿⡇⢀⢀⢀⢀⢀⣤⣀⢀⢀⢀⢀⢀");
+            System.out.println("⢀⢀ ⣶⢻⣧⣿⣿⠇ ⢸⣿⣿⣿⣷⣿⣿⣿⣷⢀⢀⢀⣾⡟⣿⡷⢀⢀⢀⢀");
+            System.out.println("⢀⢀⠈⠳⣿⣾⣿⣿⢀⠈⢿⣿⣿⣷⣿⣿⣿⣿⢀⢀⢀⣿⣿⣿⠇⢀⢀⢀⢀⢀");
+            System.out.println("⢀⢀⢀⢀⢿⣿⣿⣿⣤⡶⠺⣿⣿⣿⣷⣿⣿⣿⢄⣤⣼⣿⣿⡏⢀⢀⢀⢀⢀⢀");
+            System.out.println("⢀⢀⢀⢀⣼⣿⣿⣿⠟⢀⢀⠹⣿⣿⣿⣷⣿⣿⣎⠙⢿⣿⣿⣷⣤⣀⡀⢀⢀⢀");
+            System.out.println("⢀⢀⢀ ⢸⣿⣿⣿⡿⢀⢀⣤⣿⣿⣿⣷⣿⣿⣿⣄⠈⢿⣿⣿⣷⣿⣿⣷⡀⢀");
+            System.out.println("⢀⢀⢀⣿⣿⣿⣿⣷⣀⣀⣠⣿⣿⣿⣿⣷⣿⣷⣿⣿⣷⣾⣿⣿⣿⣷⣿⣿⣿⣆");
+            System.out.println("⣿⣿⠛⠋⠉⠉⢻⣿⣿⣿⣿⡇⡀⠘⣿⣿⣿⣷⣿⣿⣿⠛⠻⢿⣿⣿⣿⣿⣷⣦");
+            System.out.println("⣿⣿⣧⡀⠿⠇⣰⣿⡟⠉⠉⢻⡆⠈⠟⠛⣿⣿⣿⣯⡉⢁⣀⣈⣉⣽⣿⣿⣿⣷");
+            System.out.println("⡿⠛⠛⠒⠚⠛⠉⢻⡇⠘⠃⢸⡇⢀⣤⣾⠋⢉⠻⠏⢹⠁⢤⡀⢉⡟⠉⡙⠏⣹");
+            System.out.println("⣿⣦⣶⣶⢀⣿⣿⣿⣷⣿⣿⣿⡇⢀⣀⣹⣶⣿⣷⠾⠿⠶⡀⠰⠾⢷⣾⣷⣶⣿");
+            System.out.println("⣿⣿⣿⣿⣇⣿⣿⣿⣷⣿⣿⣿⣇⣰⣿⣿⣷⣿⣿⣷⣤⣴⣶⣶⣦⣼⣿⣿⣿⣷");
+        }
+    }
---
+++ test/src/main/java/test/report2.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Scanner;
+
+public class report2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("인원수를 입력하세요 : ");         //인원수 입력
+        int random = sc.nextInt();
+
+        System.out.print("총 금액을 입력하세요 : ");        //총금액 입력
+        int money = sc.nextInt();
+
+        int bboom = (int) (money / random);               //금액 나누기
+
+            System.out.println("총 금액 " + money + "원 인원수 " + random + " 명 으로 나눈금액 " + bboom + "원 입니다.");          //나눈 금액 출력
+        }
+
+    }
