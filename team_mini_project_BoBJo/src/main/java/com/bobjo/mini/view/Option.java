f2519356833b4e1e7213b8df2bc75b42ee8cbbe8
CHUWENCHING <chuuu3619@gmail.com>
2023-10-19 18:29:37 +0900

    메뉴 오타 수정 및 추가

--- team_mini_project_BoBJo/src/main/java/com/bobjo/mini/view/Option.java
+++ team_mini_project_BoBJo/src/main/java/com/bobjo/mini/view/Option.java
@@ -17,9 +17,9 @@ public class Option {
             System.out.println("||            ㅗ  ㅡ   ㅜ ㅓ   ㅁ ㅓ   ㅈ ㅣ ?　　         ||");
             System.out.println("||　 　　　　       ㄹ            ㄱ 　   　        　     ||");
             System.out.println("||                                　　                  ||");
-            System.out.println("||    　 　　    1. 올랜덤 메뉴 추천　　                    ||");
-            System.out.println("||              2. 카테고리 선택 후 랜덤 메뉴 추천 　     　 ||");
-            System.out.println("||              3. 원하는 메뉴 직접 입력 후 랜덤! 　        ||");
+            System.out.println("||    　 　　    1. 랜덤 메뉴 추천　　                      ||");
+            System.out.println("||              2. 카테고리 선택 후 랜덤 메뉴 추천 　      　||");
+            System.out.println("||              3. 원하는 메뉴 직접 입력 후 랜덤 　         ||");
             System.out.println("||              4. 한명 몰빵!           　  　           ||");
             System.out.println("||              5. 더치페이!             　 　           ||");
             System.out.println("||              6. 전체 메뉴 보기           　 　         ||");
