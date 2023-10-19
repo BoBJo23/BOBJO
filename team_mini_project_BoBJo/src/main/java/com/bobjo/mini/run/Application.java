ebd3fea47e7a5fdb5278e899e3e9a3973ad8fe70
inji-yeon <inji2349@gmail.com>
2023-10-19 17:45:54 +0900

    전체에서 랜덤 추출까지 완료

--- team_mini_project_BoBJo/src/main/java/com/bobjo/mini/run/Application.java
+++ team_mini_project_BoBJo/src/main/java/com/bobjo/mini/run/Application.java
@@ -1,12 +1,19 @@
 package com.bobjo.mini.run;
 
+import com.bobjo.mini.controller.FoodData;
+import com.bobjo.mini.controller.RandomFromAll;
 import com.bobjo.mini.view.Option;
+
+import java.util.Random;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        Option fm = new Option();
-        fm.mainmenu();
+//        Option fm = new Option();
+//        fm.mainmenu();
+
+        RandomFromAll rfa = new RandomFromAll();
+        rfa.allMenu();
     }
 }
