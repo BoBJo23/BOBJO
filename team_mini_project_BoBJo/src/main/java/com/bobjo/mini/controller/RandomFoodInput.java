0ad114dfc1fa383116cf5726d126e83830d4409c
CHUWENCHING <chuuu3619@gmail.com>
2023-10-20 14:55:29 +0900

    메뉴입력후랜덤 만듬

---
+++ team_mini_project_BoBJo/src/main/java/com/bobjo/mini/controller/RandomFoodInput.java
@@ -0,0 +1,45 @@
+package com.bobjo.mini.controller;
+
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class RandomFoodInput {
+    public void FoodPeaker() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> menuList = new ArrayList<>();
+        Random random = new Random();
+
+        System.out.println("음식 메뉴를 하나씩 입력하고 엔터를 눌러주세요! (입력이 끝난 후 \"끝\" 을 입력하세요):");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("끝")) {
+                if (menuList.isEmpty()) {
+                    System.out.println("메뉴가 입력되지 않았습니다. 다시 입력해주세요.");
+                } else {
+                    boolean keepChoosing = true;
+
+                    while (keepChoosing) {
+                        int randomIndex = random.nextInt(menuList.size());
+                        String selectedFood = menuList.get(randomIndex);
+                        System.out.println("오늘의 메뉴는 " + selectedFood + "입니다!");
+
+                        System.out.println("다시 뽑으시겠습니까? (예/아니요):");
+                        String answer = scanner.nextLine();
+
+                        if (answer.equalsIgnoreCase("아니요")) {
+                            keepChoosing = false;
+                        }
+                    }
+                }
+                break;
+            }
+
+            menuList.add(input);
+        }
+
+    }
+}
