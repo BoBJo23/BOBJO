915aef0ec868026583c4b497430dcc306baf1421
logwindy4 <logwindy4@gmail.com>
2023-10-21 03:15:31 +0900

    종철 10/21 03:15분 수정

--- team_mini_project_BoBJo/src/main/java/com/bobjo/mini/controller/Dutchpay.java
+++ team_mini_project_BoBJo/src/main/java/com/bobjo/mini/controller/Dutchpay.java
@@ -6,31 +6,41 @@ public class Dutchpay {
     public void Dutchpay() {
 
         int people = 0;
+        int money = 0;
+
         Scanner sc = new Scanner(System.in);
 
-        do{
-        System.out.print("인원수를 입력하세요 : ");
-         if (sc.hasNextInt()) {
+        do {
+            System.out.print("인원수를 입력하세요 : ");
+            if (sc.hasNextInt()) {
                 people = sc.nextInt();
 
-                if (people > 0)
+                if (people > 0) {
                     sc.nextLine();
-                     break;
-
-         } else {
-             System.out.println("유효한 숫자를 입력해 주세요.");
-             sc.nextLine();
                 }
+                break;
+            } else {
+                System.out.println("유효한 숫자를 입력해 주세요.");
+                sc.nextLine();
+            }
 
-         } while (true);
+        } while (true);
 
-        System.out.print("총 금액을 입력하세요 : ");
-            int money = sc.nextInt();
-            int bboom = (money / people);
+        do {
+            System.out.print("총 금액을 입력하세요 : ");
+            if (sc.hasNextInt()) {
+                money = sc.nextInt();
 
-            System.out.println("총 인원 " + people + "명 금액 " +  money  + " 으로 나눈금액 " + bboom + "원 입니다.");
+                if (money >= 0) {
+                    int bboom = (money / people);
+                    System.out.println("총 인원 " + people + "명 금액 " + money + " 으로 나눈금액 " + bboom + "원 입니다.");
+                }
+                break;
+                } else {
+                    System.out.println("유효한 숫자를 입력해 주세요.");
+                    sc.nextLine();
+                }
+            }while (true) ;
         }
-
     }
-
 
