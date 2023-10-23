2a0f77abc40d1078dc77e6ccc48a321ecc5e313a
22JunHee <lees8485@gmail.com>
2023-10-23 16:19:33 +0900

    2023-10-23 application 소스 수정

--- team_mini_project_BoBJo/src/main/java/com/bobjo/mini/run/Application.java
+++ team_mini_project_BoBJo/src/main/java/com/bobjo/mini/run/Application.java
@@ -1,7 +1,8 @@
 package com.bobjo.mini.run;
 import com.bobjo.mini.view.Option;
 
-
+//        Food fd = new Food();
+//        fd.foodInput();
 public class Application {
 
     public static void main(String[] args) {
