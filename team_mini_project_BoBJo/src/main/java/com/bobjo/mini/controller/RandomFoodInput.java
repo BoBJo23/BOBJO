eae9771c05633668f9da5319d78b55f61ea82179
CHUWENCHING <chuuu3619@gmail.com>
2023-10-23 17:21:14 +0900

    커밋

--- team_mini_project_BoBJo/src/main/java/com/bobjo/mini/controller/RandomFoodInput.java
+++ team_mini_project_BoBJo/src/main/java/com/bobjo/mini/controller/RandomFoodInput.java
@@ -11,31 +11,12 @@ public class RandomFoodInput {
         ArrayList<String> menuList = new ArrayList<>();
         Random random = new Random();
 
-        System.out.println("╭───────────────────────────╮");
-        String text = "3. 메뉴 직접 입력 후 랜덤";
-
-        for (int i = 0; i < text.length(); i++) {
-            System.out.print(text.charAt(i));
-            try {
-                Thread.sleep(150);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println();
-        System.out.println("╰───────────────────────────╯");
-
-        System.out.println();
-        System.out.println();
-
-        System.out.println("음식 메뉴를 하나씩 입력하고 엔터를 눌러주세요! " );
-        System.out.println( "(입력이 끝난 후 \"\u001B[34m끝\u001B[0m\" 을 입력하세요):");
+        System.out.println("음식 메뉴를 하나씩 입력하고 엔터를 눌러주세요! (입력이 끝난 후 \"끝\" 을 입력하세요):");
 
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("끝")) {
+            if (input.equals("끝")) {
                 if (menuList.isEmpty()) {
                     System.out.println("메뉴가 입력되지 않았습니다. 다시 입력해주세요.");
                 } else {
@@ -44,36 +25,12 @@ public class RandomFoodInput {
                     while (keepChoosing) {
                         int randomIndex = random.nextInt(menuList.size());
                         String selectedFood = menuList.get(randomIndex);
+                        System.out.println("오늘의 메뉴는 " + selectedFood + "입니다!");
 
-                        System.out.println();
-                        System.out.println();
-
-                        String loadingtxt = "LOADING...";
-
-                        for (int i = 0; i < loadingtxt.length(); i++) {
-                            System.out.print(loadingtxt.charAt(i));
-                            try {
-                                Thread.sleep(150);
-
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        System.out.println();
-                        System.out.println();
-                        System.out.print("오늘의 메뉴는 ");
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        System.out.println( "  "+selectedFood  + "   입니다!");
-                        System.out.println();
-                        System.out.println();
-                        System.out.println();
-                        System.out.println("다시 뽑으시겠습니까? (y/n):");
+                        System.out.println("다시 뽑으시겠습니까? (예/아니요):");
                         String answer = scanner.nextLine();
 
-                        if (answer.equalsIgnoreCase("n")) {
+                        if (answer.equals("아니요")) {
                             keepChoosing = false;
                         }
                     }
